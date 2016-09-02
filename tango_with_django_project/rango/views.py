e821412fdd034880cfeed25c39bff606a079d7f8
rayiyaoyao <rayiyaoyao@rayiyaoyaodeMacBook-Pro.local>
2016-09-02 14:04:52 +0800

    wrote an app named rango

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+	return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+	return render(request, 'rango/about.html')
+
+#	return HttpResponse("""Rango says here is the about page. <br />
+#		<a href = '/rango/'>Index</a>
+#		""")
