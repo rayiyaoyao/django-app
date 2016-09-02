9b95a232ec21f089c46b5eb1195c2b4a46e1bef7
rayiyaoyao <357837043@qq.com>
2016-09-02 18:39:01 +0800

    dynamic data to view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rango.models import Category
+
+#def index(request):
+#	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+#	return render(request, 'rango/index.html', context = context_dict)
+
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html', context = context_dict)
+
 
 def about(request):
 	return render(request, 'rango/about.html')
